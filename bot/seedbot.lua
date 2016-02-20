0407565e7a39432c4bce9119b6a9b63e1faeb118
AmirDragon13 <Khakpash36@gmail.com>
2016-02-20 22:51:34 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {187447777},--Sudo users
+    sudo_users= {187447777,187447777,187447777,187447777,tonumber},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
